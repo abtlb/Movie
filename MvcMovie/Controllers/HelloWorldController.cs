2ebfdbfe9822e55e41379928927a213b24066e44
Abdallah <ballaabotaleb@gmail.com>
2024-07-20 15:15:58 +0300

    Initial commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWord/Index/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/
+        public IActionResult Welcome(string name, string sport)
+        {
+            ViewData["Name"] = name;
+            ViewData["Sport"] = sport;
+            return View();
+        }
+    }
+}
+
